6bebd5a1193f68d4c2c7a158328a95e106e1b568
Stefan Pla <spla@icloud.com>
2025-01-16 14:35:09 -0500

    created script to move both projectiles and animals forward in the screen and applied to all prefabs

--- PlayerController.cs
+++ PlayerController.cs
@@ -3,12 +3,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
-    public float speed = 10.0f;
+    public float speed = 15.0f;
     public float xRange = 15.0f;
+    public GameObject projectilePrefab;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -19,11 +20,19 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
         // Keep Player within bounds
-        if (transform.position.x < -xRange) {
+        if (transform.position.x < -xRange)
+        {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
-        if (transform.position.x > xRange) {
-            transform.position = new Vector3(xRange, transform.position.y,transform.position.z);
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Launch projectile from the player.
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
     }
 }
