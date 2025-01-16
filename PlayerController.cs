f55846b4242758b87ebdd349416b21c85354bfef
Stefan Pla <spla@icloud.com>
2025-01-16 13:06:39 -0500

    can move player left and right, bound to a certain stretch in the x direction

--- PlayerController.cs
+++ PlayerController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float speed = 10.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +13,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = horizontalInput.GetAxis("Horizontal");
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (transform.position.x < -15) {
+            transform.position = new Vector3(-15, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > 15) {
+            transform.position = new Vector3(15, transform.position.y,transform.position.z);
+        }
     }
 }
