3dbaa57e78b45416ff9659c2345928cb1fb7e43f
Stefan Pla <spla@icloud.com>
2025-01-17 15:01:32 -0500

    added game over message to the console if an animal makes it past the player. cleaned up playercontroller script as well to break code from update method out into several other methods.

--- PlayerController.cs
+++ PlayerController.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
-    public float speed = 15.0f;
+    public float speed = 20.0f;
     public float xRange = 15.0f;
     public GameObject projectilePrefab;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -15,24 +15,43 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Move player left and right
-        horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        MovePlayer();
 
         // Keep Player within bounds
         if (transform.position.x < -xRange)
         {
-            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+            WithinBoundsLeft();
         }
         if (transform.position.x > xRange)
         {
-            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+            WithinBoundsRight();
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             //Launch projectile from the player.
-            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+            Fire();
         }
+    }
+
+    void MovePlayer()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+    }
+
+    void WithinBoundsRight() 
+    {
+        transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+    }
+
+    void WithinBoundsLeft()
+    {
+        transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+    }
+
+    void Fire()
+    {
+        Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
     }
 }
