0ca4eade007cdef8d9bc0a5d88bae73e9029ab7f
Stefan Pla <spla@icloud.com>
2025-01-17 14:33:31 -0500

    created spawnrandomanimal method, called it in start method using invokerepeating so it happens automatically at predetermined intervals

--- SpawnManager.cs
+++ SpawnManager.cs
@@ -5,22 +5,26 @@ public class NewMonoBehaviourScript : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 15.0f;
     private float spawnPosZ = 20.0f;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S)) 
-        {
-            int animalIndex = Random.Range(0,animalPrefabs.Length);
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX,spawnRangeX),0,spawnPosZ);
 
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
-        }
+    }
+
+    void SpawnRandomAnimal() 
+    {
+        int animalIndex = Random.Range(0,animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX,spawnRangeX),0,spawnPosZ);
+
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
